1d310c73b412c501b1a8aa2ad7bd44bbe11a2393
AndrewBiscuit <andrewalford1@outlook.com>
2018-04-04 13:13:43 +0100

    Added in my components

---
+++ lib/components/components.h
@@ -0,0 +1,8 @@
+//Header file to access all the different hardware components.
+
+#include "assignment_board/AssignmentBoard.h"
+#include "LEDs/LED.h"
+#include "LEDs/PWM_LED.h"
+#include "button/Button.h"
+#include "potentiometer/Potentiometer.h"
+#include "speaker/Speaker.h"
